63a27af36a473a630a7138511ab15385a4d10fa9
Jimmy Tang <jimmy.tang.360@gmail.com>
2021-11-11 13:59:05 +0000

    Using for and while loops along with arrays

---
+++ packing-list.js
@@ -0,0 +1,6 @@
+const packingList = ["tent", "bug spray", "sleeping bag"];
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
